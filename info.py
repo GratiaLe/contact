5fefcd4b9e7014203e41f57e0e14cf2b289e1dbe
GratiaLe <24567@burnside.school.nz>
2026-05-14 14:39:19 +1200

    def print all contact

--- info.py
+++ info.py
@@ -1,1 +1,13 @@
-print("Hello!")
+import sqlite3
+
+DATABASE = 'contact.db'
+
+
+def(print_all_contact)
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = 'select * from contact;'
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print(results)
+    db.close
