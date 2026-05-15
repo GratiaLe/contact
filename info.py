28d2ce56145bd898ca5c4a1a3841b2d58406bf94
GratiaLe <24567@burnside.school.nz>
2026-05-15 12:30:37 +1200

    print all contact function

--- info.py
+++ info.py
@@ -1,13 +1,23 @@
+
+print_all_contact()
+
+
 import sqlite3
 
 DATABASE = 'contact.db'
 
-
-def(print_all_contact)
+# function to print all contacts
+def print_all_contact():
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
-    sql = 'select * from contact;'
+
+    sql = 'SELECT * FROM contact;'
     cursor.execute(sql)
     results = cursor.fetchall()
-    print(results)
-    db.close
+
+    for row in results:
+        print(row)
+
+    db.close()
+
+print_all_contact()
