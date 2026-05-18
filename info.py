54e4ca124733dea4550050df3ca7e7154ffff4e3
GratiaLe <24567@burnside.school.nz>
2026-05-18 14:35:07 +1200

    def print_print_address

--- info.py
+++ info.py
@@ -5,11 +5,17 @@ DATABASE = 'contact.db'
 
 
 # function to print all contacts
-def print_all_contact():
+def print_people_address():
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
 
-    sql = 'SELECT * FROM contact;'
+    sql = """
+    SELECT person.id, person.first_name, person.last_name,
+    address.number, address.street, address.suburb
+    FROM person
+    JOIN address ON person.id = address.address_id;
+    """
+
     cursor.execute(sql)
     results = cursor.fetchall()
 
@@ -19,33 +25,4 @@ def print_all_contact():
     db.close()
 
 
-def menu():
-    while True:
-        print("\n===== CONTACT DATABASE MENU =====")
-        print("1. View all contacts")
-        print("2. Search contact")
-        print("3. Update phone number")
-        print("4. Sort contacts A-Z")
-        print("5. Filter by suburb")
-        print("6. Exit")
-
-        choice = input("Choose an option: ")
-
-        if choice == "1":
-            view_all_contacts()
-        elif choice == "2":
-            search_contact()
-        elif choice == "3":
-            update_phone()
-        elif choice == "4":
-            sort_contacts()
-        elif choice == "5":
-            filter_by_suburb()
-        elif choice == "6":
-            print("Goodbye!")
-            break
-        else:
-            print("Invalid choice, try again.")
-
-# call menu function, run program
-menu()
+print_people_address()
