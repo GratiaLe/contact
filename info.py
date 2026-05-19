7f75addec4e672834ee47fedc7ddc21d21aab6fc
GratiaLe <24567@burnside.school.nz>
2026-05-20 10:06:45 +1200

    check boundary values for search_for_person

--- info.py
+++ info.py
@@ -19,8 +19,6 @@ def print_people_address():
     cursor.execute(sql)
     results = cursor.fetchall()
 
-    print(results)
-
     for first_name, last_name, phone_number, address_id, number, street, suburb in results:
         print(f"{first_name} {last_name}, {phone_number} {address_id}, {number} {street}, {suburb}")
 
@@ -31,8 +29,17 @@ print_people_address()
 
 # function to print all contacts
 def search_for_person():
-    name = input("Enter first or last name: ")
-
+# ask user for person's foirst or last name
+    name = input("Enter first or last name: ").strip()
+# accepts error values
+    if name == "":
+        print("Error: You must enter a name.")
+        return
+# Amy is the shortest name in the person table (3 letters)
+    elif len(name) < 3:
+        print("Error: Name must be at least 2 characters.")
+        return
+    
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
 
@@ -43,5 +50,18 @@ def search_for_person():
     JOIN address ON person.id = address.address_id
     WHERE person.first_name LIKE ? OR person.last_name LIKE ?
     ''', (f"%{name}%", f"%{name}%"))
+
+    results = cursor.fetchall()
+    print("\n---SEARCH RESULTS---")
+
+    if not results:
+        print("That person was not found.")
+        db.close()
+        return
+
+    for row in results:
+        print(f"{row[0]}{row[1]}       {row[2]} {row[3]}, {row[4]} {row[5]}")
+
+    db.close()
 
 search_for_person()
