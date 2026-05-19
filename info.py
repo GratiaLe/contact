48c1367925f23efd492d50cee3b842db3040de90
GratiaLe <24567@burnside.school.nz>
2026-05-20 10:23:30 +1200

    indentation fix  for print_people_address

--- info.py
+++ info.py
@@ -37,15 +37,20 @@ def search_for_person():
         return
 # Amy is the shortest name in the person table (3 letters)
     elif len(name) < 3:
-        print("Error: Name must be at least 2 characters.")
+        print("Error: Name must be at least 3 characters.")
         return
     
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
 
     cursor.execute('''
-    SELECT person.first_name, person.last_name, person.phone_number,
-           address.number, address.street, address.suburb
+    SELECT  person.id
+            person.first_name,
+            person.last_name,
+            person.phone_number,
+            address.number,
+            address.street,
+            address.suburb
     FROM person
     JOIN address ON person.id = address.address_id
     WHERE person.first_name LIKE ? OR person.last_name LIKE ?
@@ -60,8 +65,28 @@ def search_for_person():
         return
 
     for row in results:
-        print(f"{row[0]}{row[1]}       {row[2]} {row[3]}, {row[4]} {row[5]}")
+        print(f"{row[0]}   |   {row[1]} {row[2]}   | {row[3]}, {row[4]} {row[5]} {row[6]}")
 
     db.close()
 
 search_for_person()
+
+
+# function to update phone number
+def update_phone():
+    contact_id = input("Enter contact ID to update.")
+    new_phone = input("Enter a new phone number.")
+
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+
+    cursor.execute('''
+    UPDATE contact
+    SET phone_number = ?
+    WHERE contact_id = ?
+    ''', (new_phone, contact_id))
+
+    db.commit
+    db.close()
+
+    print("Phone number updated.")
