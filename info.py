20a377cf0b295a32976f7672d0bca2fb6f4e4c2c
GratiaLe <24567@burnside.school.nz>
2026-05-19 12:19:28 +1200

    define search_for_person function

--- info.py
+++ info.py
@@ -4,7 +4,7 @@ import sqlite3
 DATABASE = 'contact.db'
 
 
-# function to print all contacts
+# function to print all contacts, and join the person adn address tables
 def print_people_address():
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
@@ -20,10 +20,26 @@ def print_people_address():
     results = cursor.fetchall()
 
 
-    for first_name, last_name, address_id, number, street, suburb in results:
-        print(f"{first_name} {last_name}, {address_id}, {number} {street}, {suburb}")
+    for first_name, last_name, phone_number, address_id, number, street, suburb in results:
+        print(f"{first_name} {last_name}, {phone_number} {address_id}, {number} {street}, {suburb}")
 
     db.close()
 
 
 print_people_address()
+
+# function to print all contacts
+def search_for_person():
+
+    name = input("Enter first or last name: ")
+
+    db = connect()
+    cursor = db.cursor()
+    
+    cursor.execute('''
+    SELECT person.first_name, person.last_name, person.phone_number, address.number, 
+            address.street, address.suburb
+    FROM person
+    JOIN address on person.id = address.address_id;
+    WHERE person.first_name LIKE ? OR person.last_name LIKE ?
+    ''', (f"%{name}%, f"%{name}%""))
